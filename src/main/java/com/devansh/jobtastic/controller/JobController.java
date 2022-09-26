b165780aa5adfb2ae2b723f0730cc3b993fd45d5
devansh-akruvala <61865437+devansh-akruvala@users.noreply.github.com>
2022-09-26 11:47:41 +0530

    Feature to post a job added

--- src/main/java/com/devansh/jobtastic/controller/JobController.java
+++ src/main/java/com/devansh/jobtastic/controller/JobController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devansh.jobtastic.models.JobPost;
@@ -19,5 +21,10 @@ public class JobController {
 	@GetMapping("/posts")
 	public List<JobPost> getAllPost(){
 		return repo.findAll();
+	}
+	
+	@PostMapping("/post")
+	public JobPost addPost(@RequestBody JobPost jobPost){
+		return repo.save(jobPost);
 	}
 }
