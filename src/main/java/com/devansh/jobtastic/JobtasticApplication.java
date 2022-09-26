c97d8a71ce55f26fe296f34030b7ac15ebe025a9
devansh-akruvala <61865437+devansh-akruvala@users.noreply.github.com>
2022-09-26 10:34:14 +0530

    Init Project

---
+++ src/main/java/com/devansh/jobtastic/JobtasticApplication.java
@@ -0,0 +1,13 @@
+package com.devansh.jobtastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devansh/jobtastic/JobtasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devansh.jobtastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
